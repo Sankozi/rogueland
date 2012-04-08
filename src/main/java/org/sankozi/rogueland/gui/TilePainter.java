6b87e9b4c40fffe2b69d1aa8918eb01377a92211
Sankozi <sankozii@gmail.com>
2012-04-08 16:30:55 +0200

    javadocs

--- src/main/java/org/sankozi/rogueland/gui/TilePainter.java
+++ src/main/java/org/sankozi/rogueland/gui/TilePainter.java
@@ -6,12 +6,27 @@ import java.awt.Rectangle;
 import org.sankozi.rogueland.control.Game;
 
 /**
- *
+ * Object that paint game field
  * @author sankozi
  */
 public interface TilePainter {
 
+	/**
+	 * Paints rectangle of certain width and height
+	 * @param game game object, can be unitialized, cannot be null
+	 * @param g graphics object
+	 * @param width width in pixels
+	 * @param height height in pixels
+	 */
     void paint(Game game, Graphics g, int width, int height);
 
+	/**
+	 * Returns rectangle representing tile at certain point
+	 * @param game game object, can be unitialized, cannot be null
+	 * @param width width in pixels
+	 * @param height height in pixels
+	 * @param location point in pixels
+	 * @return rectangle (coords in pixels)
+	 */
     Rectangle getPixelLocation(Game game, int width, int height, Point location); 
 }
