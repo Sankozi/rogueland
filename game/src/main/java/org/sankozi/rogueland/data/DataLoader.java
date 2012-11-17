f9fe76015b4b9b908deb7fa144da878db1012d98
Sankozi <sankozii@gmail.com>
2012-11-17 17:21:34 +0100

    evaluating scripts

--- game/src/main/java/org/sankozi/rogueland/data/DataLoader.java
+++ game/src/main/java/org/sankozi/rogueland/data/DataLoader.java
@@ -6,17 +6,16 @@ import com.google.common.io.CharStreams;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 /**
- *
+ * Object that loads games resources - strings, game data, and others
  * @author sankozi
  */
 public final class DataLoader {
-
-	
 
 	Collection<String> getScriptNames(){
 		List<String> ret = new ArrayList<>();
@@ -34,5 +33,9 @@ public final class DataLoader {
 		} catch (IOException ex) {
 			throw new RuntimeException(ex);
 		}
+	}
+
+	Object evaluateClResource(String name){
+		return clojure.lang.Compiler.load(new StringReader(loadResource(name)));
 	}
 }
--- game/src/test/java/org/sankozi/rogueland/data/DataLoaderTest.java
+++ game/src/test/java/org/sankozi/rogueland/data/DataLoaderTest.java
@@ -1,5 +1,7 @@
 package org.sankozi.rogueland.data;
 
+import java.util.Collection;
+import java.util.Map;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -14,6 +16,21 @@ public class DataLoaderTest {
 
 	@Test
 	public void testGetScriptNames() {
-		assert !new DataLoader().getScriptNames().isEmpty() : "no script names";
+		Collection<String> scriptNames = new DataLoader().getScriptNames();
+		assert !scriptNames.isEmpty() : "no script names";
+		assert scriptNames.contains("items.cl");
+	}
+
+	@Test
+	public void testLoadResource(){
+		String resource = new DataLoader().loadResource("items.cl");
+		assert resource.length() > 0: "resource not empty";
+	}
+
+	@Test
+	public void testItemsResource(){
+		Object res = new DataLoader().evaluateClResource("items.cl");
+		assert res != null : "items not null";
+		assert res instanceof Map : "items loads map";
 	}
 }
