3651bab68507e177f3825219fa4c98d2ef80b499
Sankozi <sankozii@gmail.com>
2013-02-03 17:38:18 +0100

    generic fix

--- game/src/main/java/org/sankozi/rogueland/data/LoaderUtils.java
+++ game/src/main/java/org/sankozi/rogueland/data/LoaderUtils.java
@@ -11,9 +11,9 @@ import java.util.Set;
  */
 class LoaderUtils {
 
-    static <K extends Enum<K>> EnumMap<K, Float> toFloatMap(Map map, Class<K> enumClass){
-        EnumMap<K, Float> ret = new EnumMap(enumClass);
-        for(Map.Entry entry : (Set<Map.Entry>)map.entrySet()){
+    static <K extends Enum<K>> EnumMap<K, Float> toFloatMap(Map<?,?> map, Class<K> enumClass){
+        EnumMap<K, Float> ret = new EnumMap<>(enumClass);
+        for(Map.Entry entry : map.entrySet()){
             ret.put(Enum.valueOf(enumClass, CaseFormat.LOWER_HYPHEN.to(CaseFormat.UPPER_UNDERSCORE, entry.getKey().toString())), 
                     ((Number)entry.getValue()).floatValue());
         }
