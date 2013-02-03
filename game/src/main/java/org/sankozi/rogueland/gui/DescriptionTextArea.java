4823da77ba7b807c2720e194392ebc197b83a675
Sankozi <sankozii@gmail.com>
2013-02-03 17:55:40 +0100

    findbugs fix

--- game/src/main/java/org/sankozi/rogueland/gui/DescriptionTextArea.java
+++ game/src/main/java/org/sankozi/rogueland/gui/DescriptionTextArea.java
@@ -12,15 +12,13 @@ import org.sankozi.rogueland.resources.ResourceProvider;
  * @author sankozi
  */
 public class DescriptionTextArea extends JTextPane {
-    Font font = ResourceProvider.getFont(Constants.STANDARD_FONT_NAME, 14f);
-    
     {
         setEditable(false);
         setContentType("text/html");
         setDocument(new CustomFontStyledDocument());
     }
 
-    private class CustomFontStyledDocument extends HTMLDocument{
+    private static class CustomFontStyledDocument extends HTMLDocument{
         @Override
         public Font getFont(AttributeSet attr) {
             return ResourceProvider.getFont(Constants.STANDARD_FONT_NAME, StyleConstants.getFontSize(attr));
