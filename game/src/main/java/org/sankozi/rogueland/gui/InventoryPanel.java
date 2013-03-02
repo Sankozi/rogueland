33e6e14348e217e1c17eacec342bb5dfda13f459
Sankozi <sankozii@gmail.com>
2013-03-02 14:39:10 +0100

    newer version of substance, correcring aliasing issues

--- game/src/main/java/org/sankozi/rogueland/gui/InventoryPanel.java
+++ game/src/main/java/org/sankozi/rogueland/gui/InventoryPanel.java
@@ -128,9 +128,9 @@ public class InventoryPanel extends JPanel implements AncestorListener, ListSele
             if(isSelected){
                 ret.setOpaque(true);
                 if(item.equipped){
-//                    ret.setBackground(Constants.BACKGROUND_EMPH_TEXT_COLOR);
+                    ret.setBackground(Constants.BACKGROUND_EMPH_TEXT_COLOR);
                 } else {
-//                    ret.setBackground(Constants.BACKGROUND_TEXT_COLOR);
+                    ret.setBackground(Constants.BACKGROUND_TEXT_COLOR);
                 }
                 ret.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2, false));
             } else if(item.equipped){
