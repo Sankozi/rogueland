56f79b9d1c7427f2b5530f5c7cc60b403fb6c7cb
Sankozi <sankozii@gmail.com>
2013-03-03 15:35:58 +0100

    growing health bar

--- game/src/main/java/org/sankozi/rogueland/gui/MainFrame.java
+++ game/src/main/java/org/sankozi/rogueland/gui/MainFrame.java
@@ -52,7 +52,7 @@ public class MainFrame extends JFrame {
         contentPane.setLayout(new MigLayout("fill, wrap 2","[grow][200!]","[grow][50!]"));
         contentPane.add(mainPanel, "span 1 2, grow");
         contentPane.add(logPanel, "grow");
-        contentPane.add(bar, "");
+        contentPane.add(bar, "grow");
         LOG.info("created MainFrame");
     }
 }
