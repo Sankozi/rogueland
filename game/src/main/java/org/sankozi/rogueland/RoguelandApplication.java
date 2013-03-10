d5102bc283a242fad949ac8ecd4023d57ecc0447
Sankozi <sankozii@gmail.com>
2013-03-10 17:41:54 +0100

    workaround used to show error messages of guice exception

--- game/src/main/java/org/sankozi/rogueland/RoguelandApplication.java
+++ game/src/main/java/org/sankozi/rogueland/RoguelandApplication.java
@@ -1,8 +1,11 @@
 package org.sankozi.rogueland;
 
+import com.google.inject.CreationException;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
+import com.google.inject.spi.Message;
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.pushingpixels.substance.api.skin.SubstanceDustCoffeeLookAndFeel;
 import org.sankozi.rogueland.gui.GuiModule;
@@ -15,6 +18,7 @@ import javax.swing.*;
  * The main class of the application.
  */
 public class RoguelandApplication {
+    private final static Logger LOG = Logger.getLogger(RoguelandApplication.class);
 
     Module module = new RoguelandModule();
 	Module guiModule = new GuiModule();
@@ -23,9 +27,20 @@ public class RoguelandApplication {
      * At startup create and show the main frame of the application.
      */
     protected void startup() {
-        Injector injector = Guice.createInjector(module, guiModule);
+        try {
+            Injector injector = Guice.createInjector(module, guiModule);
 
-        injector.getInstance(MainFrame.class).setVisible(true);
+            injector.getInstance(MainFrame.class).setVisible(true);
+        } catch (CreationException ex) {
+            LOG.error("Trying to show all guice messages...");
+            for(Message message : ex.getErrorMessages()){
+                LOG.error(message.getMessage());
+            }
+            LOG.error("...done. Exception message:");
+            LOG.error(ex.getMessage(), ex);
+        } catch (Throwable t) {
+            LOG.error(t.getMessage(), t);
+        }
     }
 
     /**
