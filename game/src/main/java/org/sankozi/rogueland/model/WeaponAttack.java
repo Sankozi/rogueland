c06536b5311b13547ea579d4193162ed78f0171d
Sankozi <sankozii@gmail.com>
2013-05-03 14:41:01 +0200

    weapon attack class

---
+++ game/src/main/java/org/sankozi/rogueland/model/WeaponAttack.java
@@ -0,0 +1,39 @@
+package org.sankozi.rogueland.model;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.sankozi.rogueland.model.coords.Direction;
+
+/**
+ * Object representing type of weapon attack.
+ *
+ * @author sankozi
+ */
+public final class WeaponAttack {
+    private final static Logger LOG = LogManager.getLogger(WeaponAttack.class);
+
+    public static enum WeaponMove {
+        SWING,
+        THRUST
+    }
+
+    private final Direction direction;
+    private final WeaponMove move;
+
+    private WeaponAttack(Direction direction, WeaponMove move) {
+        this.direction = direction;
+        this.move = move;
+    }
+
+    public static WeaponAttack get(Direction direction, WeaponMove move){
+        return new WeaponAttack(direction, move);
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public WeaponMove getMove() {
+        return move;
+    }
+}
